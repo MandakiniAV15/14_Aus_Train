0855a76347cd14964485d7a255b5304d484ca6e4
Mandakini.AV <115872874+MandakiniAV15@users.noreply.github.com>
2022-10-27 17:51:34 +0530

    Delete serializers.py

--- serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import student
-class StudentSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = student
-        fields = ['id', 'name', 'roll', 'city']
-
